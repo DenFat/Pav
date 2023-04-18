6d37e9c16f3e1341a11058e0d004b7b6a50eda23
Denis Fatkhi <zmey2257@mail.ru>
2023-04-18 10:07:56 +0300

    commit_10_07_18_04

--- Услованя конструкция/Конструкция else.py
+++ Услованя конструкция/Конструкция else.py
@@ -19,9 +19,52 @@
 # else:
 #     print('Пароль неверный')
 
-name1 = 'Маша'
-name2 = 'Марк'
-if name1 > name2:
-    print(f'{name1} больше {name2}')
-else:
-    print(f'{name2} больше {name1}')
+# name1 = 'Маша'
+# name2 = 'Марк'
+# if name1 > name2:
+#     print(f'{name1} больше {name2}')
+# else:
+#     print(f'{name2} больше {name1}')
+
+# Домашнее задание
+
+# num = int(input('Введите число '))
+# if num % 2 == 0:
+#     print('Число четное')
+# else:
+#     print('Число нечетное')
+
+# password = input('Ввкдите пароль: ')
+# pas = ['admin', 'uper', 'asdf']
+# if password in pas:
+#     print('Доступ разрешен')
+# else:
+#     print('ALLARM! ')
+#     print('we called the police')
+
+# num = int(input('Введите число: '))
+# if  num > 0:
+#     print('Введенное пользоватетелм число больше 0')
+# elif num < 0:
+#     print('Введенное пользоватетелм число меньше 0')
+# else:
+#     print('Введенное пользоватетелм число равно 0')
+
+# num1 = 0
+# ans = input('Введите значение: ')
+# if ans == 'end':
+#     num = 5
+# elif ans == '10':
+#     num = 6
+# print(num)
+
+# sub1 = int(input('Введите оценку по первому предмету: '))
+# sub2 = int(input('Введите оценку по второму предмету: '))
+# sub3 = int(input('Введите оценку по третьему предмету: '))
+# average = (sub1 + sub2 + sub3)/3
+# if average > 4.5:
+#     print(f'Твой средний бал равен {"%.1f" % average}, молодец так держать!')
+# elif 3 < average < 4.5:
+#     print(f'Твой средний бал равен {"%.1f" % average}, надо подтянуть учебу!')
+# else:
+#     print(f'Твой средний ба3л равен {"%.1f" % average}, ЭТО ОЧЕНЬ ПЛОХО!')
